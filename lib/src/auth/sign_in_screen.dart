93835c002ba976c53df44a3ca0914728dd85ae10
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-05 23:21:38 -0300

    inicio do desenvolvimento

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
