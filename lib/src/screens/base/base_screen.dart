f70daaa485431453637bb2acc947b6af834fb0f8
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-07 16:42:33 -0300

    total e confirmação do pedido

--- lib/src/screens/base/base_screen.dart
+++ lib/src/screens/base/base_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../cart/cart_tab.dart';
 import '../home/home_tab.dart';
 
 class BaseScreen extends StatefulWidget {
@@ -20,7 +21,7 @@ class _BaseScreenState extends State<BaseScreen> {
         controller: pageController,
         children: [
           const HomeTab(),
-          Container(color: Colors.yellow),
+          CartTab(),
           Container(color: Colors.blue),
           Container(color: Colors.purple),
         ],
---
+++ lib/src/screens/cart/cart_tab.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/services/utils_service.dart';
+
+class CartTab extends StatelessWidget {
+  final UtilsService utilService = UtilsService();
+  CartTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Placeholder(),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 3,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Total geral',
+                  style: TextStyle(
+                    fontSize: 12.0,
+                  ),
+                ),
+                Text(
+                  utilService.priceToCurrency(50.5),
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: CustomColors.customSwatchColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: CustomColors.customSwatchColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        )),
+                    onPressed: () {},
+                    child: const Text(
+                      'Concluir pedido',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
