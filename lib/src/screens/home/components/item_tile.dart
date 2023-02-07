a1670405c2df95e343c7735ea5d631397ccef37d
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-07 14:29:05 -0300

    adicionar o botão de carrinho e formato de preço

--- lib/src/screens/home/components/item_tile.dart
+++ lib/src/screens/home/components/item_tile.dart
@@ -2,61 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_service.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({
+  ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
   final ItemModel item;
+  final UtilsService utilsService = UtilsService();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Image.asset(item.imgUrl),
-            ),
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
-                  item.price.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: CustomColors.customSwatchColor,
-                  ),
+                Expanded(
+                  child: Image.asset(item.imgUrl),
                 ),
                 Text(
-                  '/${item.unit}',
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16.0,
                     fontWeight: FontWeight.bold,
-                    fontSize: 12.0,
                   ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      utilsService.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20.0,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
