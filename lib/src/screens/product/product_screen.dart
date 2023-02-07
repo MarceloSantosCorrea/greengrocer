278323926999d43139071851d544392eb3c2a3c5
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-07 15:08:11 -0300

    adicionando os dados do container

--- lib/src/screens/product/product_screen.dart
+++ lib/src/screens/product/product_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_service.dart';
 
 class ProductScreen extends StatelessWidget {
   final ItemModel item;
-  const ProductScreen({
+  final UtilsService utilsService = UtilsService();
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
@@ -20,6 +23,7 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: const BorderRadius.vertical(
@@ -30,6 +34,53 @@ class ProductScreen extends StatelessWidget {
                     color: Colors.grey.shade600,
                     offset: const Offset(0, 2),
                   )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          style: const TextStyle(
+                            fontSize: 27.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    utilsService.priceToCurrency(item.price),
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColors.customSwatchColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description,
+                          style: const TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('botão')),
                 ],
               ),
             ),
