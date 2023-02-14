ee1a313f76b32a3ed21783e9b63c0c2ddf51f5d3
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-14 18:23:04 -0300

    criando método de validação do token

--- lib/src/screens/auth/repository/auth_erros.dart
+++ lib/src/screens/auth/repository/auth_erros.dart
@@ -2,6 +2,8 @@ String authErrorsString(String? code) {
   switch (code) {
     case 'INVALID_CREDENTIALS':
       return 'Email e/ou senha inválidos';
+    case 'Invalid session token':
+      return 'Token Inválido';
     default:
       return 'Um erro indefinido ocorreu';
   }
--- lib/src/screens/auth/repository/auth_repository.dart
+++ lib/src/screens/auth/repository/auth_repository.dart
@@ -7,6 +7,25 @@ import 'auth_erros.dart' as auth_errors;
 class AuthRepository {
   final _httpManager = HttpManager();
 
+  Future<AuthResult> validateToken(String token) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.validateToken,
+      method: HttpMethods.post,
+      headers: {
+        'X-Parse-Session-Token': token,
+      },
+    );
+
+    if (result['result'] != null) {
+      final user = UserModel.fromJson(result['result']);
+      return AuthResult.success(user);
+    } else {
+      return AuthResult.error(
+        auth_errors.authErrorsString(result['error']),
+      );
+    }
+  }
+
   Future<AuthResult> signIn(
       {required String email, required String password}) async {
     final result = await _httpManager.restRequest(
