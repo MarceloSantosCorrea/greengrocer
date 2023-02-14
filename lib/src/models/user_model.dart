ec1f9b6abebc3082e3c1fd4afcee2bd4ecad496b
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-14 14:49:11 -0300

    criando o fromjson e tojson do usuário

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,14 +1,71 @@
+import 'dart:convert';
+
 class UserModel {
-  String name;
-  String email;
-  String phone;
-  String cpf;
-  String password;
+  String? id;
+  String? token;
+  String? name;
+  String? email;
+  String? phone;
+  String? cpf;
+  String? password;
+
   UserModel({
-    required this.name,
-    required this.email,
-    required this.phone,
-    required this.cpf,
-    required this.password,
+    this.id,
+    this.token,
+    this.name,
+    this.email,
+    this.phone,
+    this.cpf,
+    this.password,
   });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    if (id != null) {
+      result.addAll({'id': id});
+    }
+    if (token != null) {
+      result.addAll({'token': token});
+    }
+    if (name != null) {
+      result.addAll({'fullname': name});
+    }
+    if (email != null) {
+      result.addAll({'email': email});
+    }
+    if (phone != null) {
+      result.addAll({'phone': phone});
+    }
+    if (cpf != null) {
+      result.addAll({'cpf': cpf});
+    }
+    if (password != null) {
+      result.addAll({'password': password});
+    }
+
+    return result;
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      token: map['token'],
+      name: map['fullname'],
+      email: map['email'],
+      phone: map['phone'],
+      cpf: map['cpf'],
+      password: map['password'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'UserModel(id: $id, token: $token, name: $name, email: $email, phone: $phone, cpf: $cpf, password: $password)';
+  }
 }
