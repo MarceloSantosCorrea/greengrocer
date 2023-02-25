d76c97b309620f39fe3965ad4867a5c17e45f8f3
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-25 18:06:27 -0300

    criando model para a finalização de pedido

--- lib/src/models/order_model.dart
+++ lib/src/models/order_model.dart
@@ -1,13 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
 import 'cart_item_model.dart';
 
+part 'order_model.g.dart';
+
+@JsonSerializable()
 class OrderModel {
   String id;
-  DateTime createdDateTime;
+
+  DateTime? createdDateTime;
+
+  @JsonKey(name: 'due')
   DateTime overdueDateTime;
+
+  @JsonKey(defaultValue: [])
   List<CartItemModel> items;
+
   String status;
+
+  @JsonKey(name: 'copiaecola')
   String copyAndPaste;
+
   double total;
+
+  String qrCodeImage;
+
   OrderModel({
     required this.id,
     required this.createdDateTime,
@@ -16,5 +33,16 @@ class OrderModel {
     required this.status,
     required this.copyAndPaste,
     required this.total,
+    required this.qrCodeImage,
   });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OrderModelToJson(this);
+
+  @override
+  String toString() {
+    return 'OrderModel(id: $id, createdDateTime: $createdDateTime, overdueDateTime: $overdueDateTime, items: $items, status: $status, copyAndPaste: $copyAndPaste, total: $total, qrCodeImage: $qrCodeImage)';
+  }
 }
