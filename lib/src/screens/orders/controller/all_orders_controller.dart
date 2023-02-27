f6a2ef199d6e1255ca01cbc3dcd93981da8a501c
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-02-27 03:01:18 -0300

    ordenando pedidos

--- lib/src/screens/orders/controller/all_orders_controller.dart
+++ lib/src/screens/orders/controller/all_orders_controller.dart
@@ -27,7 +27,8 @@ class AllOrdersController extends GetxController {
 
     response.when(
       success: (orders) {
-        allOrders = orders;
+        allOrders = orders
+          ..sort((a, b) => b.createdDateTime!.compareTo(a.createdDateTime!));
         update();
       },
       error: (message) {
